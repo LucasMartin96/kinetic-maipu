2600a667b5fd75de09a86a20893ed2f61414d410
lmartin <lucasmartin766@gmail.com>
2025-07-15 16:42:08 -0300

    add: masstransit and serilog configuration

--- DocumentProcessor/DocumentProcessor.Worker/Program.cs
+++ DocumentProcessor/DocumentProcessor.Worker/Program.cs
@@ -1,7 +1,51 @@
+using DocumentProcessor.Worker.Consumers;
 using DocumentProcessor.Worker;
+using MassTransit;
+using Serilog;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/master/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
-var host = builder.Build();
-host.Run();
+try
+{
+
+    Log.Information("Starting Slave Worker UP...");
+
+    Host.CreateDefaultBuilder(args)
+        .UseSerilog()
+        .ConfigureServices((context, services) =>
+        {
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumer<ProcessFilesTestSlaveConsumer>();
+                x.UsingRabbitMq((ctx, cfg) =>
+                {
+                    cfg.Host("rabbitmq", "/", h =>
+                    {
+                        h.Username("user");
+                        h.Password("password");
+                    });
+
+                    cfg.ReceiveEndpoint("process-files-queue", e =>
+                    {
+                        e.ConfigureConsumer<ProcessFilesTestSlaveConsumer>(ctx);
+                    });
+                });
+            });
+
+            services.AddHostedService<Worker>();
+        })
+        .Build()
+        .Run();
+
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Slave Worker host terminated unexpectedly.");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
