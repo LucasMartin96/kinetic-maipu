e21814ae41f661ba0ae98ff9b1210875e4dcdcb6
lmartin <lucasmartin766@gmail.com>
2025-07-15 16:43:21 -0300

    add: test consumer for slave

---
+++ DocumentProcessor/DocumentProcessor.Worker/Consumers/ProcessFilesTestSlaveConsumer.cs
@@ -0,0 +1,38 @@
+﻿using DocumentProcessor.Contracts.TestsRecords;
+using MassTransit;
+
+namespace DocumentProcessor.Worker.Consumers;
+
+public class ProcessFilesTestSlaveConsumer : IConsumer<ProcessFilesTestSlaveCommand>
+{
+    private readonly ILogger<ProcessFilesTestSlaveConsumer> _logger;
+
+    public ProcessFilesTestSlaveConsumer(ILogger<ProcessFilesTestSlaveConsumer> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task Consume(ConsumeContext<ProcessFilesTestSlaveCommand> context)
+    {
+        var processId = context.Message.ProcessId;
+        var files = context.Message.Files;
+
+        _logger.LogInformation("Worker received command to process {FileCount} files for process {ProcessId}", files.Length, processId);
+
+        foreach (var file in files)
+        {
+            _logger.LogInformation("Processing file {File}", file);
+
+            await context.Publish(new FileProcessedTestEvent
+            {
+                ProcessId = processId,
+                FileName = file
+            });
+        }
+
+        await context.Publish(new ProcessCompletedTestEvent
+        {
+            ProcessId = processId
+        });
+    }
+}
