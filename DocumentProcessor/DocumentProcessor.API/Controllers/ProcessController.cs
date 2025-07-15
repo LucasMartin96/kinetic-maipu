d704c998fa62259cd72e24dbf666ac398b874b78
lmartin <lucasmartin766@gmail.com>
2025-07-15 16:42:36 -0300

    add: process controller with test method to publish messages

---
+++ DocumentProcessor/DocumentProcessor.API/Controllers/ProcessController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MassTransit;
+using DocumentProcessor.Contracts.TestsRecords;
+
+namespace DocumentProcessor.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProcessController : ControllerBase
+    {
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public ProcessController(IPublishEndpoint publishEndpoint)
+        {
+            _publishEndpoint = publishEndpoint;
+        }
+
+        [HttpPost("test")]
+        public async Task<IActionResult> TestPublish()
+        {
+            var processId = Guid.NewGuid();
+
+            await _publishEndpoint.Publish(new ProcessStartedTestEvent(processId));
+
+            return Ok(new { Message = "Event published", ProcessId = processId });
+        }
+    }
+}
