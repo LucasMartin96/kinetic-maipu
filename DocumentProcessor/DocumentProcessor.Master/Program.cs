7bb9ee1e4a627c5a488d02bd4fdeffe2f974225f
lmartin <lucasmartin766@gmail.com>
2025-07-15 16:41:30 -0300

    add: mass transit and serilog configuration for master configuration

--- DocumentProcessor/DocumentProcessor.Master/Program.cs
+++ DocumentProcessor/DocumentProcessor.Master/Program.cs
@@ -1,7 +1,49 @@
 using DocumentProcessor.Master;
+using DocumentProcessor.Master.Consumers;
+using MassTransit;
+using Serilog;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/master/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
-var host = builder.Build();
-host.Run();
+try
+{
+    Log.Information("Starting Master service...");
+
+    Host.CreateDefaultBuilder(args)
+        .UseSerilog()
+        .ConfigureServices((context, services) =>
+        {
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumer<ProcessStartedTestConsumer>();
+                x.UsingRabbitMq((ctx, cfg) =>
+                {
+                    cfg.Host("rabbitmq", "/", h =>
+                    {
+                        h.Username("user");
+                        h.Password("password");
+                    });
+
+                    cfg.ReceiveEndpoint("process-started-queue", e =>
+                    {
+                        e.ConfigureConsumer<ProcessStartedTestConsumer>(ctx);
+                    });
+                });
+            });
+
+            services.AddHostedService<Worker>();
+        })
+        .Build()
+        .Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Master service terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
