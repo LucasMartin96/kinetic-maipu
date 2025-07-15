01b3ee105b59003c3cfa4255dcd0040092fe13f8
lmartin <lucasmartin766@gmail.com>
2025-07-15 16:42:52 -0300

    add: test records just for testing

---
+++ DocumentProcessor/DocumentProcessor.Contracts/TestsRecords/ProcessStartedTestEvent.cs
@@ -0,0 +1,25 @@
+﻿namespace DocumentProcessor.Contracts.TestsRecords
+{
+    public record ProcessStartedTestEvent(Guid ProcessId);
+
+    public record ProcessFilesTestCommand
+    {
+        public Guid ProcessId { get; init; }
+        public string[] Files { get; init; }
+    }
+    public record ProcessFilesTestSlaveCommand
+    {
+        public Guid ProcessId { get; init; }
+        public string[] Files { get; init; } = Array.Empty<string>();
+    }
+
+    public record FileProcessedTestEvent
+    {
+        public Guid ProcessId { get; init; }
+        public string FileName { get; init; } = string.Empty;
+    }
+    public record ProcessCompletedTestEvent
+    {
+        public Guid ProcessId { get; init; }
+    }
+}
