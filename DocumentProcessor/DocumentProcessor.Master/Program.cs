ef419b8c568a0e99b726bc2d86386157e81b9b46
lmartin <lucasmartin766@gmail.com>
2025-07-17 00:14:08 -0300

    feat: add mysql configuration

--- DocumentProcessor/DocumentProcessor.Master/Program.cs
+++ DocumentProcessor/DocumentProcessor.Master/Program.cs
@@ -1,6 +1,9 @@
+using DocumentProcessor.Dao;
 using DocumentProcessor.Master;
 using DocumentProcessor.Master.Consumers;
 using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Polly;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
@@ -8,14 +11,23 @@ Log.Logger = new LoggerConfiguration()
     .WriteTo.File("logs/master/log.txt", rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
+
+
 try
 {
     Log.Information("Starting Master service...");
-
+    
     Host.CreateDefaultBuilder(args)
         .UseSerilog()
         .ConfigureServices((context, services) =>
         {
+            var connectionString = context.Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DocumentDbContext>(options =>
+            options.UseMySql(
+               connectionString,
+               ServerVersion.AutoDetect(connectionString))
+            );
+
             services.AddMassTransit(x =>
             {
                 x.AddConsumer<ProcessStartedTestConsumer>();
