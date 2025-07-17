d2904924ff68b7f1654cf7d3ab6343e773f2b284
lmartin <lucasmartin766@gmail.com>
2025-07-17 00:11:32 -0300

    add: db connection string.

---
+++ DocumentProcessor/DocumentProcessor.Dao/Entities/ProcessSagaState.cs
@@ -0,0 +1,18 @@
+﻿namespace DocumentProcessor.Dao.Entities;
+
+public class ProcessSagaState
+{
+    public Guid CorrelationId { get; set; }
+
+    public Guid ProcessId { get; set; }
+
+    public string CurrentState { get; set; } = string.Empty;
+
+    public int TotalFiles { get; set; }
+    public int CompletedFiles { get; set; }
+    public int FailedFiles { get; set; }
+    public int SkippedFiles { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
