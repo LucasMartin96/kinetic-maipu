a28acd4cc6f71e03abd75de5e19f71ff37f1ec7d
lmartin <lucasmartin766@gmail.com>
2025-07-21 14:41:18 -0300

    add migrations

---
+++ DocumentProcessor/DocumentProcessor.Dao/Migrations/20250721160046_AddFolderPathAndIncreaseStatusLength.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentProcessor.Dao.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFolderPathAndIncreaseStatusLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PersistedFiles",
+                table: "ProcessSagaStates",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "ProcessSagaStates",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Processes",
+                type: "varchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldMaxLength: 20)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FolderPath",
+                table: "Processes",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PersistedFiles",
+                table: "ProcessSagaStates");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "ProcessSagaStates");
+
+            migrationBuilder.DropColumn(
+                name: "FolderPath",
+                table: "Processes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Processes",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(64)",
+                oldMaxLength: 64)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
