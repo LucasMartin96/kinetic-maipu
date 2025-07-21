db99e750c4413aa2191b9ab306b8c6087d648f4b
lmartin <lucasmartin766@gmail.com>
2025-07-21 14:31:10 -0300

    fix: the json was giving me problems with thevolume.. so just like this

--- DocumentProcessor/DocumentProcessor.Worker/Services/StopWordsFilter.cs
+++ DocumentProcessor/DocumentProcessor.Worker/Services/StopWordsFilter.cs
@@ -13,50 +13,62 @@ namespace DocumentProcessor.Worker.Services
 
         public async Task<HashSet<string>> LoadStopWordsAsync()
         {
-            var stopWords = new HashSet<string>();
-            
-            var possiblePaths = new[]
+            var stopWords = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
             {
-                Path.Combine("/app/config/stopwords.json")
+                "a", "acá", "ahí", "al", "algo", "algunas", "alguno", "algunos", "allá", "allí", "ambos", "ante", "antes",
+                "aquel", "aquella", "aquellas", "aquello", "aquellos", "aquí", "así", "aun", "aunque", "bajo", "bien", "cabe",
+                "cada", "casi", "cierta", "ciertas", "cierto", "ciertos", "como", "con", "conmigo", "conseguimos", "conseguir",
+                "consigo", "consigue", "consiguen", "consigues", "contigo", "contra", "cual", "cuales", "cualquier", "cualquiera",
+                "cualquieras", "cuan", "cuando", "cuanta", "cuantas", "cuanto", "cuantos", "de", "dejar", "del", "demás", "demasiada",
+                "demasiadas", "demasiado", "demasiados", "dentro", "desde", "donde", "dos", "el", "él", "ella", "ellas", "ello",
+                "ellos", "empleais", "emplean", "emplear", "empleas", "empleo", "en", "encima", "entonces", "entre", "era", "erais",
+                "eramos", "eran", "eras", "eres", "es", "esa", "esas", "ese", "eso", "esos", "esta", "estaba", "estabais", "estaban",
+                "estabas", "estad", "estada", "estadas", "estado", "estados", "estais", "estamos", "estan", "estando", "estar",
+                "estaremos", "estará", "estarán", "estarás", "estaré", "estaréis", "estaríamos", "estarían", "estarías", "estas",
+                "este", "estemos", "estén", "estés", "esto", "estos", "estoy", "estuve", "estuviera", "estuvierais", "estuvieran",
+                "estuvieras", "estuvieron", "estuviese", "estuvieseis", "estuviesen", "estuvieses", "estuvimos", "estuviste",
+                "estuvisteis", "estuviéramos", "estuviésemos", "estuvo", "ex", "excepto", "fue", "fuera", "fuerais", "fueran",
+                "fueras", "fueron", "fuese", "fueseis", "fuesen", "fueses", "fui", "fuimos", "fuiste", "fuisteis", "gran", "grandes",
+                "gueno", "ha", "habéis", "había", "habíais", "habíamos", "habían", "habías", "haber", "habremos", "habrá", "habrán",
+                "habrás", "habré", "habréis", "habría", "habríais", "habríamos", "habrían", "habrías", "habéis", "habíamos", "había",
+                "hace", "haceis", "hacemos", "hacen", "hacer", "haces", "hacia", "hago", "han", "has", "hasta", "hay", "haya", "hayamos",
+                "hayan", "hayas", "he", "hemos", "hicieron", "hizo", "hoy", "hubiera", "hubierais", "hubieran", "hubieras", "hubieron",
+                "hubiese", "hubieseis", "hubiesen", "hubieses", "hubimos", "hubiste", "hubisteis", "hubiéramos", "hubiésemos", "hubo",
+                "igual", "incluso", "intenta", "intentais", "intentamos", "intentan", "intentar", "intentas", "intento", "ir", "jamás",
+                "junto", "juntos", "la", "largo", "las", "le", "les", "lo", "los", "luego", "mal", "más", "me", "menos", "mi", "mía",
+                "mías", "mientras", "mío", "míos", "mis", "misma", "mismas", "mismo", "mismos", "modo", "mucha", "muchas", "muchísima",
+                "muchísimas", "muchísimo", "muchísimos", "mucho", "muchos", "muy", "nada", "ni", "ninguna", "ningunas", "ninguno",
+                "ningunos", "no", "nos", "nosotras", "nosotros", "nuestra", "nuestras", "nuestro", "nuestros", "nunca", "o", "os",
+                "otra", "otras", "otro", "otros", "para", "parece", "parte", "pero", "poca", "pocas", "poco", "pocos", "podemos",
+                "poder", "podrá", "podrán", "podría", "podrían", "poner", "por", "por qué", "porque", "primero", "puede", "pueden",
+                "puedo", "pues", "que", "qué", "querer", "quien", "quienes", "quiere", "quién", "quiénes", "quizá", "quizás", "sabe",
+                "sabes", "salvo", "se", "sea", "seamos", "sean", "seas", "según", "ser", "seremos", "será", "serán", "serás", "seré",
+                "seréis", "sería", "seríais", "seríamos", "serían", "serías", "si", "sí", "sido", "siendo", "sin", "sino", "so", "sobre",
+                "sois", "solamente", "solo", "somos", "soy", "su", "sus", "suya", "suyas", "suyo", "suyos", "tal", "tales", "también",
+                "tampoco", "tan", "tanta", "tantas", "tanto", "tantos", "te", "tendrá", "tendrán", "tendré", "tendréis", "tendría",
+                "tendríais", "tendríamos", "tendrían", "tendrías", "tened", "tenemos", "tener", "tenga", "tengamos", "tengan", "tengas",
+                "tengo", "tenida", "tenidas", "tenido", "tenidos", "tenéis", "tenía", "teníais", "teníamos", "tenían", "tenías", "ti",
+                "tiempo", "tiene", "tienen", "tienes", "toda", "todas", "todavía", "todo", "todos", "tomar", "trabaja", "trabajais",
+                "trabajamos", "trabajan", "trabajar", "trabajas", "trabajo", "tras", "tú", "tu", "tus", "tuve", "tuviera", "tuvierais",
+                "tuvieran", "tuvieras", "tuvieron", "tuviese", "tuvieseis", "tuviesen", "tuvieses", "tuvimos", "tuviste", "tuvisteis",
+                "tuviéramos", "tuviésemos", "tuvo", "tuya", "tuyas", "tuyo", "tuyos", "un", "una", "unas", "uno", "unos", "usted",
+                "ustedes", "va", "vais", "valor", "vamos", "van", "varias", "varios", "vaya", "veces", "ver", "verdad", "verdadera",
+                "verdadero", "vez", "vosotras", "vosotros", "voy", "vuestra", "vuestras", "vuestro", "vuestros", "y", "ya", "yo"
             };
 
-            foreach (var path in possiblePaths)
+            // Por si las dudas...
+            var gutenbergHeaders = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
             {
-                try
-                {
-                    if (File.Exists(path))
-                    {
-                        _logger.LogInformation("WORKER: Found stop words file at: {Path}", path);
-                        var stopWordsJson = await File.ReadAllTextAsync(path);
-                        var stopWordsList = JsonSerializer.Deserialize<List<string>>(stopWordsJson);
-                        
-                        if (stopWordsList != null)
-                        {
-                            stopWords = new HashSet<string>(stopWordsList, StringComparer.OrdinalIgnoreCase);
-                            _logger.LogInformation("WORKER: Successfully loaded {Count} stop words from {Path}", stopWords.Count, path);
-                            
-                            _logger.LogInformation("WORKER: Key stop words check - 'de': {HasDe}, 'que': {HasQue}, 'no': {HasNo}, 'se': {HasSe}, 'del': {HasDel}", 
-                                stopWords.Contains("de"), stopWords.Contains("que"), stopWords.Contains("no"), stopWords.Contains("se"), stopWords.Contains("del"));
-                            
-                            return stopWords;
-                        }
-                    }
-                    else
-                    {
-                        _logger.LogInformation("WORKER: Stop words file not found at: {Path}", path);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogWarning(ex, "Failed to load stop words from {Path}", path);
-                }
-            }
-
-            _logger.LogWarning("WORKER: No stop words file found, using default list");
-            stopWords = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
-            {
-                "el", "la", "que", "y", "a", "en", "un", "es", "se", "no", "te", "lo", "le", "da", "su", "por", "son", "con", "para", "al", "del", "los", "las", "una", "como", "pero", "sus", "me", "hasta", "hay", "donde", "han", "quien", "están", "estado", "desde", "todo", "nos", "durante", "todos", "podido", "tres", "tan", "así", "veinte", "año", "uno", "ya", "poco", "he", "esa", "poco", "esa", "en", "otro", "algunos", "algunas", "ser", "dos", "también", "era", "eran", "vez", "tienen", "esa", "porque", "muy", "sin", "sobre", "también", "mi", "contra", "solo", "han", "yo", "hay", "vez", "pueden", "todos", "así", "nos", "de"
+                "project", "gutenberg", "ebook", "e-book", "this", "book", "is", "for", "the", "use", "of", "anyone", "anywhere",
+                "united", "states", "most", "other", "parts", "world", "at", "no", "cost", "and", "with", "almost", "restrictions",
+                "whatsoever", "you", "may", "copy", "it", "give", "away", "re-use", "under", "terms", "license", "included", "online",
+                "www.gutenberg.org", "located", "check", "laws", "country", "before", "using", "title", "author", "translator",
+                "release", "date", "language", "credits", "university", "toronto", "internet", "archive", "distributed",
+                "proofreading", "team", "produced", "images", "made", "available", "hathitrust", "digital", "library",
+                "start", "end", "of", "the", "project", "gutenberg", "ebook", "el", "crimen", "y", "castigo"
             };
+
+            stopWords.UnionWith(gutenbergHeaders);
 
             return stopWords;
         }
