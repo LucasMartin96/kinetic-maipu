c7fe708dab9e87e8fa6d48bfa95ff5eb601afa31
lmartin <lucasmartin766@gmail.com>
2025-07-20 22:34:57 -0300

    add: migrations !!!

--- DocumentProcessor/DocumentProcessor.Dao/Migrations/20250720201902_AddProcessTimestamps.Designer.cs
+++ DocumentProcessor/DocumentProcessor.Dao/Migrations/20250720201902_AddProcessTimestamps.Designer.cs
@@ -4,6 +4,7 @@ using DocumentProcessor.Dao;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DocumentProcessor.Dao.Migrations
 {
     [DbContext(typeof(DocumentDbContext))]
-    partial class DocumentDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250720201902_AddProcessTimestamps")]
+    partial class AddProcessTimestamps
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
